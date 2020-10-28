a3f165bf3a213e9ef256daa4c73833dbe26023e9
Evan Byrd <evanbyrd16@gmail.com>
2020-10-28 15:09:54 -0400

    Added text input and a button that can reference the text.

--- App.js
+++ App.js
@@ -1,11 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, Alert} from 'react-native';
 
 export default function App() {
+  const [userEmail, emailText] = React.useState('');
+  const [userPassword, passwordText] = React.useState('');
+
   return (
     <View style={styles.container}>
-      <Text>Hello World!</Text>
+      <TextInput
+        style={styles.TextInput}
+        placeholder='User Email'
+        onChangeText={text => emailText(text)}
+        value={userEmail}
+      />
+      <TextInput
+        style={styles.TextInput}
+        placeholder='User Password'
+        onChangeText={text => passwordText(text)}
+        value={userPassword}
+      />
+      <Button
+        title="Submit"
+        onPress={() => Alert.alert(userEmail + ' ' + userPassword)}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +32,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    padding: 10,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  TextInput: {
+    padding: 10,
+    height: 40,
+    width: '70%',
+    borderColor: 'gray',
+    borderWidth: 1,
+    margin: 5
+  }
 });
